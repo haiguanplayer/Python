30456709b2f6947c5b820692f9f2e95283b5d046
huatian <1909641489@qq.com>
2017-06-07 16:14:28 +0800

    learn use python
    python code

---
+++ basic learning/hackerrank String Split and Join.py
@@ -0,0 +1,10 @@
+s = input()
+s = s.split(" ")
+s = "-".join(s)
+print (s)
+'''
+print (input().replace(" ","-"))
+
+
+print('-'.join(input().split()))
+'''
---
+++ basic learning/hackerrank swapcase.py
@@ -0,0 +1,11 @@
+s = input()
+res = "";
+for c in s:
+    if c.islower():
+        res += c.upper();
+    else:
+        res += c.lower();
+print (res)
+'''
+print (input().swapcase());
+'''
